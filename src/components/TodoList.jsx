d0ada70202b58c8788bda579e0881deb9a7408a4
Markus Väänänen <markus.vaananen@gmail.com>
2024-03-05 08:45:05 +0200

    Floating filters

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,34 +1,54 @@
 import { useState } from "react";
-import TodoTable from './TodoTable'
+import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-material.css"; // Optional Theme applied to the grid
+
 
 function TodoList() {
 
     const [todo, setTodo] = useState({
         desc:'',
-        date:''
+        date:'',
+        priority:''
     });
 
     const [todos, setTodos] = useState([]);
 
+    const [colDefs, setColDefs] = useState([
+        { field:'desc', filter:true, editable:true, floatingFilter: true },
+        { field:'priority', filter:true, floatingFilter: true,
+            cellStyle: params => params.value === 'High' ? {color:'red'} : {color:'black'} 
+        },
+        { field:'date', filter:true, floatingFilter: true }
+    ]);
+
     const handleClick = () => {
-        if (todo.desc && todo.date) {
+        if (todo.desc && todo.date && todo.priority) {
             setTodos([...todos, todo]);
-            setTodo({desc:'', date:''});
+            setTodo({desc:'', date:'', priority:''});
         } else {
             alert('Type values first');
         }
     }
 
     const deleteTodo = (index) => {
-        setTodos( todos.filter((todo, i) => i !== index) );
+        setTodos( todos.filter((_, i) => i !== index) );
     }
 
     return(
         <>
+
+
             <input
                 placeholder="Description"
                 value={todo.desc}
                 onChange={e => setTodo( {...todo, desc: e.target.value } )}
+            />
+
+            <input
+                placeholder="Priority"
+                value={todo.priority}
+                onChange={e => setTodo( {...todo, priority: e.target.value } )}
             />
 
             <input
@@ -40,7 +60,20 @@ function TodoList() {
 
             <button onClick={handleClick}>Add Todo</button>
 
-            <TodoTable todos={todos} deleteTodo={deleteTodo}/>
+
+            <div 
+                className='ag-theme-material' 
+                style={ {height:600, width:650}}
+            >
+                <AgGridReact 
+                    rowData={todos}
+                    columnDefs={colDefs}
+                />
+
+
+            </div>
+
+
         </>
     );
 
--- src/components/TodoTable.jsx
+++ src/components/TodoTable.jsx
@@ -1,29 +1,29 @@
 function TodoTable(props) {
     return(
-        <>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Description</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        props.todos.map( (todo, i) => 
-                            <tr key={i}>
-                                <td>{todo.date}</td>
-                                <td>{todo.desc}</td>
-                                <td>
+    
+        <table>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    props.todos.map( (todo, i) => 
+                        <tr key={i}>
+                            <td>{todo.date}</td>
+                            <td>{todo.desc}</td>
+                            <td>
                                 <button onClick={()=>props.deleteTodo(i)}>Delete</button>
-                                </td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </table>
-        </>
+                            </td>
+                        </tr>
+                    )
+                }
+            </tbody>
+        </table>
+
     );
 }
 
