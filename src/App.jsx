8e7d13b2973599ed93466b1b420b3a3570188cce
Markus Väänänen <markus.vaananen@gmail.com>
2024-03-11 22:27:29 +0200

    Material design date picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 
 import './App.css'
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import TodoList from './components/TodoList'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 
 function App() {
-
   return (
     <>
-      <h3>My Todos</h3>
-      <TodoList />
+
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">My Todos</Typography>
+          </Toolbar>
+        </AppBar>
+        <TodoList />
+
+      </LocalizationProvider>
+      
     </>
   )
-
 }
 
 export default App
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-material.css"; // Optional Theme applied to the grid
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 
 function TodoList() {
@@ -22,6 +26,8 @@ function TodoList() {
         { field:'date', filter:true, floatingFilter: true }
     ]);
 
+    const gridRef = useRef();
+
     const handleClick = () => {
         if (todo.desc && todo.date && todo.priority) {
             setTodos([...todos, todo]);
@@ -31,46 +37,60 @@ function TodoList() {
         }
     }
 
-    const deleteTodo = (index) => {
-        setTodos( todos.filter((_, i) => i !== index) );
+    const handleDelete = () => {
+        if (gridRef.current.getSelectedNodes().length > 0) {
+            setTodos( todos.filter((todo, i) => i != gridRef.current.getSelectedNodes()[0].id) );
+        } else {
+            alert("Select a row first!");
+        }
+    }
+
+    const handleDate = date => {
+        const dateStr = date.toISOString();
+        console.log(  dateStr );
+        setTodo( {...todo, date: dateStr } );
     }
 
     return(
         <>
+            <Stack 
+                direction="row" 
+                spacing={2} 
+                justifyContent="center" 
+                alignItems="center"
+                mt={2}
+            >
+                <TextField
+                    label="Description"
+                    value={todo.desc}
+                    onChange={e => setTodo( {...todo, desc: e.target.value } )}
+                />
 
+                <TextField
+                    label="Priority"
+                    value={todo.priority}
+                    onChange={e => setTodo( {...todo, priority: e.target.value } )}
+                />
 
-            <input
-                placeholder="Description"
-                value={todo.desc}
-                onChange={e => setTodo( {...todo, desc: e.target.value } )}
-            />
+                <DatePicker value={todo.date} onChange={ e => handleDate(e) } 
+                    label="Date"
+                    views={['year', 'month', 'day']}
+                />
 
-            <input
-                placeholder="Priority"
-                value={todo.priority}
-                onChange={e => setTodo( {...todo, priority: e.target.value } )}
-            />
-
-            <input
-                type="date"
-                placeholder="Date"
-                value={todo.date}
-                onChange={e => setTodo( {...todo, date: e.target.value } )}
-            />
-
-            <button onClick={handleClick}>Add Todo</button>
-
-
+                <Button variant='contained' onClick={handleClick}>Add Todo</Button>
+                <Button variant='contained' color='error' onClick={handleDelete}>Delete</Button>
+            </Stack>
             <div 
                 className='ag-theme-material' 
                 style={ {height:600, width:650}}
             >
                 <AgGridReact 
+                    ref={gridRef}
+                    onGridReady={params => gridRef.current = params.api}
                     rowData={todos}
                     columnDefs={colDefs}
+                    rowSelection="single"
                 />
-
-
             </div>
 
 
